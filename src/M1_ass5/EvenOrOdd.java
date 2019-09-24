147dd9dda941e47cfce7d67f67e2984584f09d4a
srimrunalini <thotamrunalini8@gmail.com>
2019-09-24 12:04:21 +0530

    initial commit: simply add the file

---
+++ src/M1_ass5/EvenOrOdd.java
@@ -0,0 +1,28 @@
+package M1_ass5;
+
+import javax.swing.*;
+
+/**
+ * Given n, return true if n is even otherwise false.
+ * 
+ * @author Siva Sankar
+ */
+
+public class EvenOrOdd {
+
+    /**
+     * This method returns true if it is an even number false otherwise.
+     * @param n, the integer
+     * @return true if n is even and false otherwise.
+     */
+    public static boolean evenOrOdd(int n) {
+        //  Your code goes here....
+        if(n%2!=0){
+        //  
+            return false;}
+        else
+            return true;
+
+
+    }
+}
