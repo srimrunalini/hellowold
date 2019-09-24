2a75ca19277e6f42cd2aeb82ebd93c9f135ed4b1
srimrunalini <thotamrunalini8@gmail.com>
2019-09-24 17:44:40 +0530

    initial commit: simply add the file

---
+++ src/M2_ass6/InTenTwenty.java
@@ -0,0 +1,37 @@
+package M2_ass6;
+
+/**
+ * Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+ * 
+ * Input : in1020(12, 99)
+ * Output : true
+ * 
+ * Input : in1020(21, 12)
+ * Output : true
+ * 
+ * Input : in1020(8, 99)
+ * Output : false
+ * 
+ * @author Siva Sankar
+ */
+
+ public class InTenTwenty {
+
+    /**
+     * This method should return true if either of the tow numbers as parameters are in the
+     * range of 10..20 inclusive, false otherwise.
+     * 
+     * @param a, the first integer as parameter to the method.
+     * @param b, the second integer as parameter to the method.
+     * 
+     * @return true if either of them is in the range of 10..20 inclusive and 
+     * false otherwise.
+     */
+    public static boolean in1020(int a, int b) {
+        // Your code goes here....
+        if((a<=20 && a>=10) || (10>=b && b<=20)){
+            return true;}
+        else{
+            return false;}
+        }
+    }
